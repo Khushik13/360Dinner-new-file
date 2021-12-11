c247e2d85638b44f21a36f4ff70731ffce741fcd
Khushik13 <mailtokhushi13@gmail.com>
2021-12-11 20:31:05 +0530

    publish

---
+++ Fullstack/Frontend/Languages/js/3ways0foutput/main.js
@@ -0,0 +1,38 @@
+<script>
+
+class Humans{
+    constructor(){
+       this. eyes=2
+    this.nose=1
+   this.hairs=true
+    }
+  dance(){
+
+  }
+}
+
+var partik=new Humans()
+console.log(partik)
+
+
+
+
+class Mobile{
+    constructor(){
+       this. brandname
+    this.screen
+   this.buttons
+   this.touch
+    }
+   
+
+}
+
+
+
+
+
+
+
+
+</script>
